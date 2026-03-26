3f8b36a7fc5a16689a73a929968c2edf40b6aa6c
ameen8012 <alameen@bizpole.in>
2026-03-26 14:40:26 +0530

    Update DirectorPromoterForm.jsx

--- src/components/ExixistingCompany/DirectorPromoterForm.jsx
+++ src/components/ExixistingCompany/DirectorPromoterForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { upsertCompany } from "../../api/CompanyApi";
 import { getSecureItem } from "../../utils/secureStorage";
 
